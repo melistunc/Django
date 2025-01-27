899f31fc198614052bdc55e3c8a4ed07cd9368a6
melis <131777538+melistunc@users.noreply.github.com>
2025-01-27 21:27:24 +0300

    Add files via upload

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE sqltest (id INTEGER PRIMARY KEY, name VARCHAR, age INT, instrument VARCHAR);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
